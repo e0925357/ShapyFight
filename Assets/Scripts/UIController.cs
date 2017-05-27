cbe717c027282b117a255326acf0c1e9d484737f
LAPTOP-528R07CB\kikob <kiko.balaj@gmail.com>
2017-05-27 12:23:57 +0200

    New Enemies, UI, GameController

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CanvasGroupExtension;
+using UnityEngine.UI;
+using System.Text;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    [SerializeField]
+    private CanvasGroup failedScreenCanvasGroup;
+    [SerializeField]
+    private CanvasGroup HUDCanvasGroup;
+    [SerializeField]
+    private RectTransform boostBarFillerRect;
+    [SerializeField]
+    private Text scoreText;
+
+    private StringBuilder sb;
+
+    private void Awake()
+    {
+        instance = this;
+        sb = new StringBuilder(13);
+    }
+
+    private void Start()
+    {
+        UpdateBoostBar();
+    }
+
+    public void UpdateBoostBar()
+    {
+        boostBarFillerRect.localScale = new Vector3(GameController.instance.BoostValue - 1, 1, 1);
+    }
+
+    public void UpdateScoreText()
+    {
+        sb.Append(string.Format("Score: {0}", GameController.instance.Score));
+        scoreText.text = sb.ToString();
+        sb.Remove(0, sb.Length);
+    }
+
+    public void ShowFailedUI()
+    {
+        failedScreenCanvasGroup.Enable();
+        HUDCanvasGroup.Disable();
+    }
+
+    public void ShowHUDUI()
+    {
+        failedScreenCanvasGroup.Disable();
+        HUDCanvasGroup.Enable();
+    }
+}
