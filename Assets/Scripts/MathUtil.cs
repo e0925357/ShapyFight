971c6322d948061d6f77a5d85b1d775d0e676b90
Stefan Zaufl <stefan.zaufl@hotmail.com>
2017-05-28 11:07:25 +0200

    Speed boost is now scaled + added script for text animation

---
+++ Assets/Scripts/MathUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Math
+{
+	public static class MathUtil
+	{
+		public static bool eq(float f1, float f2)
+		{
+			return f1 == f2 || Mathf.Approximately(f1, f2);
+		}
+
+		/// <summary>
+		/// An easing function with a quadratic in/out smoothing ranging from 0 to 1 in both x and y.
+		/// The Integral of this function between 0 and 1 is 0.5
+		/// </summary>
+		/// <param name="t">The time parameter of the easing, should be between 0 and 1</param>
+		/// <returns>The eased value between 0 and 1.</returns>
+		public static float quadraticEasingInOut(float t)
+		{
+			if (t < 0.5f) return 4f*t*t/2f;
+			else return (2f - (2f*t - 2f)*(2f*t - 2f))/2f;
+		}
+	}
+}
---
+++ Assets/Scripts/PointsDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+	public class PointsDisplay : MonoBehaviour
+	{
+		[SerializeField]
+		private Text pointsDisplay;
+		[SerializeField]
+		private AnimationCurve scaleEffect;
+		
+		private Coroutine lastCoroutine = null;
+
+		public void DoScaleEffect() {
+			lastCoroutine = StartCoroutine(doScaleEffect());
+		}
+
+		IEnumerator doScaleEffect()
+		{
+			if (lastCoroutine != null)
+			{
+				StopCoroutine(lastCoroutine);
+			}
+
+			float maxTime = scaleEffect.keys[scaleEffect.length - 1].time;
+			Vector3 scale = transform.localScale;
+			float timer = 0;
+
+			do
+			{
+				scale.x = scaleEffect.Evaluate(timer);
+				scale.y = scaleEffect.Evaluate(timer);
+
+				transform.localScale = scale;
+
+				timer += Time.deltaTime;
+				yield return new WaitForEndOfFrame();
+			} while (timer < maxTime);
+
+			scale = Vector3.one;
+			transform.localScale = scale;
+			lastCoroutine = null;
+		}
+	}
+}
